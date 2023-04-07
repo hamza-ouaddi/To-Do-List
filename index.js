56e4ea17834a6edd69191e55362314b28c90cb1c
Hamza Ouâddi <hamza0addi@gmail.com>
2023-04-07 22:11:04 +0000

    Add database, modifying and updating the website functions in back-end

--- index.js
+++ index.js
@@ -1,15 +1,42 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const _ = require("lodash");
 const bodyParser = require("body-parser");
+require("dotenv").config();
 
 //Exporting date module which has the function "getDate()"
 const date = require(__dirname + "/date.js");
 
-const port = 3000;
+const port = process.env.PORT;
 const app = express();
 
-//The item that added by the user
-const items = [];
-const workItems = [];
+//////////////////////////////////MongoDB//////////////////////////////////
+
+//Connecting to Mongoose database
+mongoose.connect("mongodb://127.0.0.1:27017/todolistDB");
+
+//Item Schema
+const itemSchema = new mongoose.Schema({
+  description: {
+    type: String,
+    required: true,
+  },
+});
+
+//Item Model
+const Item = mongoose.model("Item", itemSchema);
+
+const defaultItems = [];
+
+//Custom List Schema
+const listSchema = new mongoose.Schema({
+  title: String,
+  items: [itemSchema],
+});
+
+//Custom List Model
+const List = mongoose.model("List", listSchema);
+//////////////////////////////////////////////////////////////////////////
 
 //To render using EJS
 app.set("view engine", "ejs");
@@ -18,17 +45,61 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", function (req, res) {
-  res.render("main", { listTitle: "To Do List" });
+  res.render("main", {});
 });
 //Post request whenever a user type and add a new item and push it to the list
 app.post("/", function (req, res) {
-  const item = req.body.newItem;
-  if (req.body.list === "Work List") {
-    workItems.push(item);
-    res.redirect("/work");
+  const itemDescription = req.body.newItem;
+  const listTitle = req.body.list;
+  const day = date.getDate();
+
+  const item = new Item({
+    description: itemDescription,
+  });
+
+  if (listTitle === day) {
+    item.save();
+    res.redirect("/today");
   } else {
-    items.push(item);
-    res.redirect("/tobuy");
+    List.findOne({ title: listTitle })
+      .then((foundList) => {
+        foundList.items.push(item);
+        foundList.save();
+        res.redirect("/" + listTitle);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+});
+
+//Delete
+app.post("/delete", function (req, res) {
+  const checkedItemId = req.body.checkbox;
+  const listTitle = req.body.listTitle;
+  const day = date.getDate();
+
+  if (listTitle === day) {
+    Item.findByIdAndRemove(checkedItemId)
+      .then(() => {
+        console.log("Successfully deleted");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+    res.redirect("/today");
+  } else {
+    List.findOneAndUpdate(
+      { title: listTitle },
+      { $pull: { items: { _id: checkedItemId } } }
+    )
+      .then(() => {
+        res.redirect("/" + listTitle);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }
 });
 
@@ -38,19 +109,52 @@ app.get("/button.js", function (req, res) {
 });
 
 //To buy list pages
-app.get("/tobuy", function (req, res) {
+app.get("/today", function (req, res) {
   const day = date.getDate();
-  res.render("list", { listTitle: day, newListItems: items });
+  find();
+  async function find() {
+    await Item.find({})
+      .then((foundItems) => {
+        res.render("list", { listTitle: day, newListItems: foundItems });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
 });
 
-//Work list page
-app.get("/work", function (req, res) {
-  res.render("list", { listTitle: "Work List", newListItems: workItems });
+//Generate custom list pages
+app.get("/:customListTitle", function (req, res) {
+  const customListTitle = _.capitalize(req.params.customListTitle);
+
+  List.findOne({ title: customListTitle })
+    .then((foundList) => {
+      if (!foundList) {
+        //Create new list
+        const list = new List({
+          title: customListTitle,
+          items: defaultItems,
+        });
+        list.save();
+        res.redirect("/" + customListTitle);
+      } else {
+        //Show the existing list
+        res.render("list", {
+          listTitle: foundList.title,
+          newListItems: foundList.items,
+        });
+      }
+    })
+    .catch((err) => {
+      confirm.log(err);
+    });
 });
 
-//About page
-app.get("/about", function (req, res) {
-  res.render("about");
+//Create a new list
+app.post("/create", function (req, res) {
+  let listTitle = req.body.newListTitle;
+
+  res.redirect("/" + listTitle);
 });
 
 app.listen(port, function () {
--- public/buttons.js
+++ public/buttons.js
@@ -1,31 +1,50 @@
 //to choose one of the two lists "To Do List" & "To Work List"
 const buyBtn = document.getElementById("buy-btn");
 const workBtn = document.getElementById("work-btn");
+const customizeBtn = document.getElementById("customize-btn");
+const customizeInput = document.getElementById("customize-part");
+const Btn = document.getElementById("customize-btn");
 
 if (window.location.pathname === "/") {
   buyBtn.onclick = function () {
-    location.assign("/tobuy");
+    location.assign("/today");
   };
 
-  workBtn.onclick = function () {
-    location.assign("/work");
-  };
+  customizeBtn.addEventListener("click", () => {
+    customizeInput.classList.toggle("show");
+  });
 }
 
 //To change the check icon and item list style when the user click on
 let listItems = document.querySelectorAll(".list-item");
 
 if (
-  window.location.pathname === "/tobuy" ||
+  window.location.pathname === "/today" ||
   window.location.pathname === "/work"
 ) {
   listItems.forEach((listItem) => {
     let checkBtn = listItem.querySelector(".check-btn");
+    let deletebtn = listItem.querySelector(".checkbox-btn");
     let paragraph = listItem.querySelector(".paragraph");
     checkBtn.addEventListener("click", () => {
       checkBtn.classList.toggle("fa-solid");
       checkBtn.classList.toggle("fa-circle-check");
       checkBtn.classList.toggle("fa-circle");
+      deletebtn.classList.toggle("show");
+      paragraph.classList.toggle("listitem-checked");
+      listItem.classList.toggle("item-checked");
+    });
+  });
+} else {
+  listItems.forEach((listItem) => {
+    let checkBtn = listItem.querySelector(".check-btn");
+    let paragraph = listItem.querySelector(".paragraph");
+    let deletebtn = listItem.querySelector(".checkbox-btn");
+    checkBtn.addEventListener("click", () => {
+      checkBtn.classList.toggle("fa-solid");
+      checkBtn.classList.toggle("fa-circle-check");
+      checkBtn.classList.toggle("fa-circle");
+      deletebtn.classList.toggle("show");
       paragraph.classList.toggle("listitem-checked");
       listItem.classList.toggle("item-checked");
     });
